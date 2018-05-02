2dea630cd85a5a98537bf535d584c9c6de8233f4
xuhongxu96 <xuhongxu96@gmail.com>
2018-05-02 16:43:31 +0800

    Added Jieba Segmenter.

---
+++ XiaoyaNLP/TextSegmentation/JiebaSegmenter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using XiaoyaNLP.Encoding;
+using XiaoyaNLP.Helper;
+
+namespace XiaoyaNLP.TextSegmentation
+{
+    public class JiebaSegmenter : ITextSegmenter
+    {
+        protected static JiebaNet.Segmenter.JiebaSegmenter sSegmenter 
+            = new JiebaNet.Segmenter.JiebaSegmenter();
+        protected const int MaxSegmentLength = 15;
+
+        protected IEnumerable<TextSegment> SegmentChineseSentence(string sentence, int baseIndex)
+        {
+            foreach (var token in sSegmenter.Tokenize(sentence, JiebaNet.Segmenter.TokenizerMode.Search))
+            {
+                yield return new TextSegment
+                {
+                    Word = token.Word,
+                    Position = baseIndex + token.StartIndex,
+                    Length = token.Word.Length,
+                };
+            }
+        }
+
+        public IEnumerable<TextSegment> Segment(string sentence)
+        {
+            var match = CommonRegex.AnyChars.Match(sentence);
+
+            while (match.Success)
+            {
+                var subSentence = match.Value;
+                var index = match.Index;
+
+                if (CommonRegex.ChineseChars.IsMatch(subSentence))
+                {
+                    foreach (var segment in SegmentChineseSentence(subSentence, index))
+                    {
+                        if (segment.Length <= MaxSegmentLength)
+                        {
+                            yield return segment;
+                        }
+                    }
+                }
+                else
+                {
+                    if (subSentence.Length <= MaxSegmentLength)
+                    {
+                        yield return new TextSegment
+                        {
+                            Word = subSentence,
+                            Position = index,
+                            Length = subSentence.Length,
+                        };
+                    }
+                }
+
+                match = match.NextMatch();
+            }
+
+        }
+    }
+}
---
+++ XiaoyaNLPUnitTest/TextSegmentation/JiebaSegmenterUnitTest.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using XiaoyaNLP.TextSegmentation;
+
+namespace XiaoyaNLPUnitTest.TextSegmentation
+{
+    [TestClass]
+    public class JiebaSegmenterUnitTest
+    {
+        [TestMethod]
+        public void TestJiebaSegmenter()
+        {
+            var segmenter = new JiebaSegmenter();
+            var segments = segmenter.Segment("1.2中文分词中华人民共和国Hello World!你\n好，我 们2.2.3.4");
+            var words = segments.Select(o => o.Word);
+
+            Assert.IsTrue(words.Contains("1.2"));
+            Assert.IsTrue(words.Contains("中文"));
+            Assert.IsTrue(words.Contains("分词"));
+            Assert.IsTrue(words.Contains("中华人民共和国"));
+            Assert.IsTrue(words.Contains("你"));
+            Assert.IsTrue(words.Contains("好"));
+            Assert.IsTrue(words.Contains("我"));
+            Assert.IsTrue(words.Contains("们"));
+            Assert.IsTrue(words.Contains("Hello"));
+            Assert.IsTrue(words.Contains("World"));
+            Assert.IsTrue(words.Contains("2.2"));
+            Assert.IsTrue(words.Contains("3.4"));
+
+            foreach (var segment in segments)
+            {
+                Console.WriteLine(string.Format("{0}: {1}", segment.Word, segment.Position));
+            }
+        }
+
+        [TestMethod]
+        public void TestJiebaSegmenterMultiThread()
+        {
+            var segmenter = new JiebaSegmenter();
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < 1000; ++i)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    var segments = segmenter.Segment("1.2中文分词中华人民共和国Hello World!你\n好，我 们2.2.3.4");
+                    var words = segments.Select(o => o.Word);
+
+                    Assert.IsTrue(words.Contains("1.2"));
+                    Assert.IsTrue(words.Contains("中文"));
+                    Assert.IsTrue(words.Contains("分词"));
+                    Assert.IsTrue(words.Contains("中华人民共和国"));
+                    Assert.IsTrue(words.Contains("你"));
+                    Assert.IsTrue(words.Contains("好"));
+                    Assert.IsTrue(words.Contains("我"));
+                    Assert.IsTrue(words.Contains("们"));
+                    Assert.IsTrue(words.Contains("Hello"));
+                    Assert.IsTrue(words.Contains("World"));
+                    Assert.IsTrue(words.Contains("2.2"));
+                    Assert.IsTrue(words.Contains("3.4"));
+
+                    foreach (var segment in segments)
+                    {
+                        Console.WriteLine(string.Format("{0}: {1}", segment.Word, segment.Position));
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+        }
+    }
+}
