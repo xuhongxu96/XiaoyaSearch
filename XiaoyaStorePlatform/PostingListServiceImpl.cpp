84b7b190951cc9cb008eef6c6f326d61ef3630cd
xuhongxu96 <xuhongxu96@gmail.com>
2018-05-28 13:17:15 +0800

    Finished grpc for PostingListService.

---
+++ XiaoyaStorePlatform/PostingListServiceImpl.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "PostingListServiceImpl.h"
+
+using namespace grpc;
+using namespace XiaoyaStore::Service;
+using namespace XiaoyaStore::Store;
+using namespace XiaoyaStore::Model;
+
+PostingListServiceImpl::PostingListServiceImpl(Store::PostingListStore & store)
+	:mStore(store)
+{ }
+
+Status PostingListServiceImpl::SavePostingList(ServerContext * context,
+	const ArgPostingList * request, Result * response)
+{
+	mStore.SavePostingList(request->postinglist());
+	response->set_is_successful(true);
+
+	return Status::OK;
+}
+
+Status PostingListServiceImpl::GetPostingList(ServerContext * context,
+	const ArgWord * request, ResultWithPostingList * response)
+{
+	response->set_is_successful(
+		mStore.GetPostingList(request->word(), *response->mutable_postinglist())
+	);
+
+	return Status::OK;
+}
---
+++ XiaoyaStorePlatform/PostingListServiceImpl.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "stdafx.h"
+#include "PostingListStore.h"
+
+namespace XiaoyaStore
+{
+	namespace Service
+	{
+		class PostingListServiceImpl final
+			: public PostingListService::Service
+		{
+			Store::PostingListStore &mStore;
+		public:
+			PostingListServiceImpl(Store::PostingListStore &store);
+			::grpc::Status SavePostingList(::grpc::ServerContext* context, const ArgPostingList* request, Result* response);
+			::grpc::Status GetPostingList(::grpc::ServerContext* context, const ArgWord* request, ResultWithPostingList* response);
+		};
+	}
+}
